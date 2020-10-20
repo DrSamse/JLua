2a59a52f7452a58d75599e8a4f99525d646561f1
DrSamse <samuel.noessi@gmail.com>
2020-10-20 16:51:46 +0200

    Seperating TestSources

--- JLua/__init__.py
+++ JLua/__init__.py
@@ -2,35 +2,6 @@ from typing import Type
 from lupa._lupa import LuaRuntime
 import lupa._lupa
 
-class Test():
-    var1 = 10
-    var2 = "Test"
-    def __init__(self):
-        pass
-
-    def Test(self):
-        pass
-
-    def __repr__(self):
-        return "var1 = " + str(self.var1) + "  var2 = " + self.var2
-
-testDictionary = {
-    "var1" : 30,
-    "var2" : 50,
-    "testClass" : Test(),
-    "test" : {
-        "moreTest" : "Success",
-        "extraTest" : {
-
-        },
-        "listTest" : [ 10, 20, "Nice" ]
-    }
-}
-
-otherDict = {
-    1, 2, 3, 4
-}
-
 def SumString(table : list) -> str:
     sumstring = ""
     for string in table: sumstring += string
